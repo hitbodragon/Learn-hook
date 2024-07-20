f4b42f71691d06c0f9b8a82422be20828bdc9503
hitbodragon <longtrong02@mail.com>
2024-07-20 17:21:52 +0700

    setTimeout, setInterval, useEffect, cleanup

--- src/Content.js
+++ src/Content.js
@@ -1,21 +1,9 @@
-// useEffect
-// kiến thức cần có
-// Event : Add/ remove event listener
-// Observer pattern : SubScribe /unsubscribe// mở rộng của add và remove listener
-// Closure
 // Times setInterval , SetTimeout,ClearInterval, clearTimeout
-//useState
-//Mouse/unmouse
-//===
+
 //call api
 
 /**
- * 1. Update DOM
- * -F8 blog tittle
- * 2. Call API
- * 3 Listen DOM events
- * -Scroll
- * -Resize
+
  * 4.Cleanip
  * -Remove Listener / Unsubscribe
  * Clear time
@@ -23,33 +11,40 @@
 /// lý thuyết chung của 3 cái học bên dưới
 //  1. Callbacks luôn được gọi khi component mounted(cài vào)
 // 2. Clearn fuction luôn luôn được gọi trước khi component unmounted(gỡ ra)
+// 3 clearnup được gọi trước khi compoment được goi trừ lần mounted
+
+// times setInterval, SetTimeoutInterval,
+// 1 hoat dong trong react component
+// 2 là set nó như thế nào
+// 3.cách giải quyết
+//xây đựng 1 ứng dụng dếm ngược theo giây
+// setTimeout chạy 1 lần duy nhất theo số s đẫ được lập trình
+// còn setInterval sét nhìu lần chạy  theo số s đã được lên lịch trình
 import { useEffect, useState } from 'react';
 
-// clean up resize basic
-//hiện ra kích thước cửa sổ trình duyệt và update lại nó
 function Content() {
-    const [width, setWidth] = useState(window.innerWidth);
-
+    const [countDown, setCountDown] = useState(190);
+    // cách dùng sai
+    // setInterval(() => {
+    //     setCountDown(countDown - 1);
+    // }, 1000);
+    // lặp vô hạn
+    // mỗi 1s chạy nhiều lần set lại cùng 1 lúc
+    // do chạy 1 lần nên chọn cách số 2
+    // setTimes out chạy có 1 lần nên khi số thay đôi thì ta truyền des cho nó chạy tiiếp
     useEffect(() => {
-        const handleResize = () => {
-            setWidth(window.innerWidth);
-        };
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+        const clearSet = setInterval(() => {
+            //setCountDown(countDown - 1); // chạy được 1 lần dừng lại nhưn nó vẫn cahyj setinterval ngầm bên trong
+            setCountDown((prevCount) => prevCount - 1); // nhưng callback cho phép nó chạy render ra két quả
+            console.log('setInterval');
+        }, 1000);
+
+        return () => clearInterval(clearSet);
+    }, []); // do ngoặc ko có giá trị nên chạy đúng 1 lần render giao diện
     return (
         <div>
-            <h1> {width} </h1>
+            <h1> {countDown} </h1>
         </div>
     );
 }
 export default Content;
-
-// chia nhỏ ra học
-//1 là useEffect( callbacks,)
-//- Gọi callbacks mỗi khi component re-render
-//- gọi callback sau khi thêm element vào dom
-//2 là useEffect(callbacks,[])
-// chỉ gọi callbacks 1 lần sau khi component mounted, logic chạy 1 lần
-//3 là useEffect(callbacks,[deps]
-// callbacks sẽ được gọi lại mỗi khi deps thay đổi
