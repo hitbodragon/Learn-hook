80acadf6ab5a3831cf969bcc151745f2321c97f2
hitbodragon <longtrong02@mail.com>
2024-07-20 11:21:13 +0700

    two-way biding

--- src/App.js
+++ src/App.js
@@ -1,80 +1,126 @@
-//useState muốn dữ liệu thay đổi thì giao diện tự động  được cập nhật(rende lại theo dữ liệu )
+import { useState } from 'react';
 
-import { useCallback, useState } from 'react';
-//vd vơi innit state callback
-const orders = [1000, 10, 20];
+// Lưu ý
+//-1. Array
+//-2. Reference
+
+//ví dụ
+//1.RamdomGift
+//2.Two-way Bingding
+//3.Todolist
+
+// 1 ramdom gifts
+//const gifts = ['CPU', 'RAM 32 RGB', 'RGB Keyboard'];
+
+//2 two-way biding vi du ve radio checkbox
+const courses = [
+    {
+        id: 1,
+        name: 'HTML',
+    },
+    {
+        id: 2,
+        name: 'CSS',
+    },
+    {
+        id: 3,
+        name: 'JS',
+    },
+];
+
 function App() {
-    // đầu vào là gì đầu ra là gì và cách vận hành nó ra sao
-    // useState nhận giá trị khi khởi tạo đầu vào
-    // giá trị khởi tạo dùng được chỉ 1 lần duy nhất
-    // state đầu tiên lấy giá trị khởi tạo  nó dưa cho state
-    // phần tử setState là 1 cái hàm xét lại giá trị
+    // 1 ramdomgift
+    // const [gift, setGifts] = useState('');
 
-    // khi bấm vào nó sẽ chạy hàm handler sau đó đoạn code bên trong được thực thi từ đó giá trị mà hàm trước đó để giá trị mặt định được render ra ngoài
-
-    // example ứng dụng đếm số tăng dần
-    // component được render lại sau khi dùng setState
-    // initial State chỉ dùng cho lần đầu
-
-    // Lưu ý :
-    // -Component được  render sau khi 'setState'
-    // - Initial state chỉ dùng cho lần đầu
-    // -Set State với useCallback
-    // -Initial state  với call back
-    // -setState là thay thế state bằng giá trị mới
-
-    //-Initial state  với call tocall back
-    // const total = orders.reduce((total, cur) => total + cur);
-    // console.log(total);
-    // const [count, setCount] = useState(() => {
-    //     const total = orders.reduce((total, cur) => total + cur);
-    //     console.log(total); // no se khong tinh laij nhu o tren tang hieu nag cuar web
-    //     return total;
-    // });
-    // const handleCount = () => {
-    //     // setCount(count + 1);
-    //     // // 1 setState đơn giản
-    //     // setCount(count + 1);
-    //     // gọi nhìu lần nó cũng chỉ render lại 1 lần duy nhất thôi
-    //     // //2 sử dụng call back để gọi ra nhìu lần
-    //     // preState trong trường hợp này là count, coi như SetCount là 1 hàm và nhận count là giá trị khởi tạo
-    //     setCount((preState) => preState + 1);
-    //     // setCount((preState) => preState + 1);
-    //     // setCount((preState) => preState + 1);
+    // const randomGifts = () => {
+    //     const indexRandom = Math.floor(Math.random() * gifts.length);
+    //     setGifts(gifts[indexRandom]);
     // };
-    // console.log('render-re');
-    // // nhưng render vẫn 1 lần
     // return (
-    //     <div className="App" stye={{ padding: '20' }}>
-    //         <h1>{count}</h1>
-    //         <button onClick={handleCount}> Increase </button>
-    //         {/* cach 2 laf ()=> setCount(count+1) */}
+    //     <div style={{ padding: '20' }}>
+    //         {/* {có gitf lấy gìt ko có lấy chưa có phâng thưởng} */}
+    //         <h1> {gift || 'Chưa có phần thưởng '} </h1>
+    //         <button onClick={randomGifts}> Lấy thưởng</button>
     //     </div>
     // );
-    // destucturing đi với rest
-    // vi du voi object
-    //spread lấy [ mảng trùn tên], key trùng tên  lấy tên sau
 
-    const [info, setInfo] = useState({
-        name: 'John',
-        age: 30,
-        address: '123 Street',
-    });
-    // the gioi tinnh vao
-    const handleCount = () => {
-        // setInfo({
-        //     ...info,
-        //     females: 'nam',
-        // });
-        setInfo((pre) => ({
-            ...pre,
-            females: 'name',
-        }));
+    // 2. two way binding
+
+    // ràng buộc 1 chiều là giá trị thay đổi trong quá trình render
+    // ràng buộc 2 chiều là giá trị có thể thay đổi ngay khi gõ code  trong components
+    // 2 bên ràng buộc với nhau  , mất đi 1 chiều chiều kia sử dụng là two way binding
+    //2. vi dụ về text don gian
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const handleSubmit = () => {
+    //     console.log({
+    //         name,
+    //         email,
+    //     });
+    // };
+    // vi dụ ve radio với check box
+    // const [checked, setChecked] = useState(2);
+
+    // const handleSubmit = () => {
+    //     console.log({ id: checked });
+    // };
+    const [checked, setChecked] = useState([]);
+
+    const handleChecked = (id) => {
+        setChecked((prev) => {
+            const isChecked = checked.includes(id);
+            if (isChecked) {
+                return checked.filter((item) => item !== id);
+            } else {
+                return [...prev, id];
+            }
+        });
     };
+
+    const handleSubmit = () => {
+        console.log({ id: checked });
+    };
+    // // return (
+    // //     <div style={{ padding: '20px' }}>
+    //         {/*  vi du don gian ve text */}
+    //         {/* <input
+    //             type="text"
+    //             onChange={(e) => setName(e.target.value)}
+    //             value={name}
+    //         />
+    //         <input
+    //             type="text"
+    //             onChange={(e) => setEmail(e.target.value)}
+    //             value={email}
+    //         /> */}
+    //         {/* vi dụ về radio */}
+    //         {/* {courses.map((course, index) => (
+    //             <div key={index}>
+    //                 <input
+    //                     type="radio"
+    //                     // kiem tra dieu kieenj luc dau check == undefined luc sau nos laf 1 nen 2 cai bang nhau nen check in ra , check = ban dau mac dinh la true dung nó sẽ in ra
+    //                     checked={checked === course.id}
+    //                     onChange={() => setChecked(course.id)}
+    //                 />
+    //                 {course.name}
+    //             </div>
+    //         ))} */}
     return (
-        <div style={{ padding: '20' }}>
-            <h1> {JSON.stringify(info)} </h1>
-            <button onClick={handleCount}>Change Info</button>
+        <div>
+            {courses.map((course, index) => (
+                <div key={index}>
+                    <input
+                        type="checkbox"
+                        // kiem tra dieu kieenj luc dau check == undefined luc sau nos laf 1 nen 2 cai bang nhau nen check in ra , check = ban dau mac dinh la true dung nó sẽ in ra
+                        // includes trả về true nếu nó có giá trị chứa bên trong , vd giá trị id trong bài
+                        checked={checked.includes(course.id)}
+                        onChange={() => handleChecked(course.id)}
+                    />
+                    {course.name}
+                </div>
+            ))}
+
+            <button onClick={handleSubmit}>Register</button>
         </div>
     );
 }
