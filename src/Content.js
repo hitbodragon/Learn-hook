ed50e11a2ee94882afc292ef866293815d4857f1
hitbodragon <longtrong02@mail.com>
2024-07-20 18:41:46 +0700

    useEffect with fake Chat App

--- src/Content.js
+++ src/Content.js
@@ -1,34 +1,44 @@
 import { useEffect, useState } from 'react';
-// bai toan import images lên trang web sau đó dọn dẹp lại luôn
+
+// xử lý chức năng  thời gian thực
+const lessons = [
+    {
+        id: 1,
+        name: 'React là gì',
+    },
+    {
+        id: 2,
+        name: 'State và Effect',
+    },
+    {
+        id: 3,
+        name: 'Props và State',
+    },
+];
 function Content() {
-    // cần ảnh hiện thay đổi re-render lại nên cần dùng state
-    const [avatar, setAvatar] = useState();
-    const handlePreviewAvatar = (e) => {
-        const file = e.target.files[0];
-        //console.log(URL.createObjectURL(file));
-        // preview tu dat ten
-        file.preview = URL.createObjectURL(file); // url xem tạm
-
-        setAvatar(file); // set vẫn còn lưu trong bộ nhớ
-    };
+    const [lessonId, setLessonId] = useState(1);
     useEffect(() => {
-        // dùng luôn cleanup dể xoá bỏ cái ảnh thừa
-
-        return () => {
-            // kiem tra avatar co ton tai khong moi xoa, chứ mặt dịnh nó là undefined
-            avatar && URL.revokeObjectURL(avatar.preview); // nhận cái ủl xem tạm đó  để xoá url xem tạm khỏi bộ nhớ
+        const handleComment = ({ detail }) => {
+            console.log(detail);
         };
-    }, [avatar]); // mỗi lần chọn ảnh nó sẽ chọt vào cái này
-
+        window.addEventListener(`lesson-${lessonId}`, handleComment);
+        return () => {
+            window.removeEventListener(`lesson-${lessonId}`, handleComment);
+        };
+    }, [lessonId]);
     return (
         <div>
-            <input
-                type="file"
-                // multiple // cho phép chọn nhiều ảnh còn ko có cho phép chọn 1 ảnh duy nhất mà thôi
-                onChange={handlePreviewAvatar} // onchange là sự thay đổi nên 2 hình 1 lúc nó sẽ ko hiện nha
-            />
-            {avatar && <img src={avatar.preview} alt="" width="80%" />}
-            {/* hiển thị ảnh đã chọn */}
+            {lessons.map((lesson) => (
+                <li
+                    key={lesson.id}
+                    style={{
+                        color: lessonId === lesson.id ? 'red' : '#333',
+                    }}
+                    onClick={() => setLessonId(lesson.id)}
+                >
+                    {lesson.name}
+                </li>
+            ))}
         </div>
     );
 }
