81acc09134765a42d2fbc082fafb5c5fea7bfcd9
hitbodragon <longtrong02@mail.com>
2024-07-21 11:09:43 +0700

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import Content from './Content';
 
 function App() {
     const [count, setCount] = useState(0);
-    const [count2, setCount2] = useState(0);
 
-    const increase = () => {
-        // mỗi lần re render gọi lại hàm sẽ dẫn dến việc tham chiếu bị thay đổi
-        setCount(count + 1);
-    };
-
-    const increase2 = () => {
-        setCount2(count2 + 1);
-    };
+    const handleIncrease = useCallback(() => {
+        setCount((prevCount) => prevCount + 1);
+    }, []);
 
     return (
-        <div style={{ padding: '10px 32px' }}>
-            <Content count={count} />
+        <div style={{ padding: '10px 32px', color: '#f05123' }}>
+            <Content onIncrease={handleIncrease} />
             <h1>{count}</h1>
-            <h1>{count2}</h1>
-            <button onClick={increase}>Click me!</button>
-            <button onClick={increase2}>Click me 2! </button>
         </div>
     );
 }
 
 export default App;
+// useCallback tránh viện rerender của các component ko cần thiết
+// sau khi  thực hiện lệnh button các hàm sẽ thực hiện lệnh thì các hàm sẽ tả về 1 giá trị tham chiếu và lưu vào giá trị lưu trữ,
+// các biến này có giá trị lưu trữ lưu vào trong bộ nhớ cho nên dù gọi 1 hàm giống nhau nhưng tham chiếu khác nhau dẫn dến memo k phan biệt được
+// dẫn dến trường hợp re render các component ko cần thiết dẫn đến hiệu năng trang web kém
+// trong trường hợp đó usecallback ra đời và giúp mỗi lần funtion thực hiện thì usecallback sẽ lưu giá trị tham chiếu đó vào bên trong nó
+// mỗi khi gọi hàm giá trị tham chiếu trong usecallback ko thay đổi vì đã lưu ở trước đó,, qua hàm children nó sẽ được memo so sánh bằng toán tử ====
+// và giá trị là như nhau nên các component sẽ ko render ko cần thiết nữa
--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,14 @@
 import { memo } from 'react';
 
-function Content({ count }) {
+function Content({ onIncrease }) {
     console.log('re-render');
 
-    return <h2>SỐ LƯỢNG CLICK CỦA BẠN LÀ : {count}</h2>; // props cha thay đổi nên con cũng thay đổi
+    return (
+        <>
+            <h2>HELLO ANH EM TRÊN F8</h2>
+            <button onClick={onIncrease}>Click me!</button>
+        </>
+    );
 }
 
 export default memo(Content);
-// memo giúp cho việc re render không cần thiết : nếu như các props của cha thay đổi thì hàm con sẽ thay đổi theo
-//tránh việc các props cha ko thay đổi mà con cứ  rerender hiệu năng nó sẽ giảm
-// sụwr dụng toán tử  === so sánh , nếu cha thay đổi nhưng thay đổi giống nhau ko qua props dến con thì con ko bị rerender lại
-// nếu bên cha thay đổi ít nhất 1 pops thì nó sẽ rerender lại
