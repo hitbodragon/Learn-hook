e20980137f9bfa8d69a0bd51fc8e2660cacdfb73
hitbodragon <longtrong02@mail.com>
2024-07-21 10:39:16 +0700

    update useEffectLayout

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,23 @@
 import { useLayoutEffect, useState } from 'react';
 
+// #useEffect
+// 1. Cập nhật lại state
+// 2. Cập nhật DOM (mutated)
+// 3. Render lại UI
+// 4. Gọi cleanup nếu deps thay đổi
+// 5. Gọi useEffect callback
+
+// #useLayoutEffect
+// 1. Cập nhật lại state
+// 2. Cập nhật DOM (mutated)
+// 3. Gọi cleanup nếu deps thay đổi (sync)
+// 4. Gọi useLayoutEffect callback (sync)
+// 5. Render lại UI
 function Content() {
     const [count, setCount] = useState(0);
 
     useLayoutEffect(() => {
+        // thực hiện callback trước chứ ko phải chờ render rồi mới thực hiện
         if (count > 3) setCount(0);
     }, [count]);
 
