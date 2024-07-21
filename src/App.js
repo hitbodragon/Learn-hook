a31710b488ce1439f3035ac945da2e2215e66d29
hitbodragon <longtrong02@mail.com>
2024-07-21 21:50:15 +0700

    useReducer

--- src/App.js
+++ src/App.js
@@ -18,38 +18,108 @@ import Content from './Content';
 //4 dispatch
 
 // bài làm
-//1 Init State
-const initState = 0;
+//1 Init State, 2 state tạo 1 cái object
+const initState = {
+    job: '',
+    jobs: [],
+};
 //Actions
-const UP_ACTION = 'up';
-const DOWN_ACTION = 'down';
+const SET_JOB = 'set_job';
+const ADD_JOB = 'add_job';
+const DELETE_JOB = 'delete_job';
+
+// payload dữ liệu mang theo đi
+const setJob = (payload) => {
+    return {
+        type: SET_JOB,
+        payload,
+    };
+};
+const addJob = (payload) => {
+    return {
+        type: ADD_JOB,
+        payload,
+    };
+};
+const deleteJob = (payload) => {
+    return {
+        type: DELETE_JOB,
+        payload,
+    };
+};
+
 // 3 reducers (hàm)
 // (init , action), nhận đầu vào là giá trị khởi tạo và nhân action làm hành động, giá trị hiện tại và hành động là gì
 const reducer = (state, action) => {
-    console.log('running');
-    // action trong switch// giá trị bảo lưu dựa vào init state
-    switch (action) {
-        case UP_ACTION:
-            return state + 1;
-        case DOWN_ACTION:
-            return state - 1;
+    console.log('action', action);
+    console.log('prev state', state); // trước khi update
+    let newState;
+    //action trong switch// giá trị bảo lưu dựa vào init state
+    switch (action.type) {
+        case SET_JOB:
+            newState = {
+                ...state, // state là init cũ của job vào trong jobs
+                job: action.payload, // payload là dữ liệu
+            };
+            break;
+        case ADD_JOB:
+            newState = {
+                ...state,
+                jobs: [...state.jobs, action.payload], // bảo  lưu và thêm giá trị mới vào mảng
+            };
+            break;
+        case DELETE_JOB:
+            const newJobs = [...state.jobs]; // giaỉ tất cả phần tử vào mảng mới
+            newJobs.splice(action.payload, 1); // splice xoá 1 phần tử trong mảng
+            newState = {
+                ...state,
+                jobs: newJobs,
+            };
+            break;
         default:
             throw new Error(`Invalid action ${action}`); // action ko hợp lệ
     }
+    console.log('new state', newState);
+    return newState;
 };
 function App() {
     // useReduce có thể nhận lên đến 3 đối số (reudcer, initState)
     // chạy , lần đầu chạy gọi reducer để đó , nhận giá trị khởi tạo và return lại 1 giá trị array
     // trong trường hợp này count nhận initState làm giá trị khởi tạo , giá trị thứ 2 là 1 hmaf gọi là dispatch
     // dispatch dùng để kích hoạt 1 cái action xem nó có hoạt động hay ko
-    const [count, dispatch] = useReducer(reducer, initState);
-
+    const [state, dispatch] = useReducer(reducer, initState);
+    const inputRef = useRef();
+    const { job, jobs } = state;
     //usememo(callback,des)
+    const handleSubmit = () => {
+        dispatch(addJob(job));
+        dispatch(setJob('')); // sau khi add xong thì reset lại input
+        inputRef.current.focus();
+    };
     return (
         <div style={{ padding: '10px 20px' }}>
-            <h1>{count}</h1>
-            <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
-            <button onClick={() => dispatch(UP_ACTION)}>Up</button>
+            <h1>Todo</h1>
+            <input
+                ref={inputRef}
+                type="text"
+                value={job} // toway binding, input phụ thuộc vào dữ liệu r, nên muốn thay đổi cần có sự kiện
+                placeholder=" Enter Todo"
+                onChange={(e) => {
+                    dispatch(setJob(e.target.value)); // lay ca du lieu va co hanh dong truyen vao luon
+                }}
+            />
+            <button onClick={handleSubmit}>ADD</button>
+            <ul>
+                {jobs.map((job, index) => (
+                    <li key={index}>
+                        {job}
+                        <span onClick={() => dispatch(deleteJob(index))}>
+                            {/* do xoá mảng nên dùng index, ko có index sài id */}
+                            &times;
+                        </span>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
